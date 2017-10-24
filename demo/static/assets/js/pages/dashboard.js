9242195aba635e401ce12e7f75f689776454602c
Cartmanishere <apbetahouse45@gmail.com>
2017-10-24 11:33:13 +0530

    die

--- demo/static/assets/js/pages/dashboard.js
+++ demo/static/assets/js/pages/dashboard.js
@@ -1,8 +1,6 @@
 $( document ).ready(function() {
     
-    setTimeout(function(){ Materialize.toast('Welcome to Alpha!', 4000) }, 4000);
-    setTimeout(function(){ Materialize.toast('You have 4 new notifications', 4000) }, 11000);
-    
+
     
     
     // CounterUp Plugin
